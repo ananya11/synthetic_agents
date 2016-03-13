45cb4c6587002bbeb594db9e75fc234ff78ea416
ananya11 <ananya11@users.noreply.github.com>
2016-03-13 08:00:51 -0400

    Update DbConnections.py

--- DbConnections.py
+++ DbConnections.py
@@ -4,21 +4,21 @@ import cx_Oracle
 def create_connection(db):
 	connStr = None
 	if db.lower() == 'protopop':   #oracle
-		connStr = 'uname/pwd@noldor-db.vbi.vt.edu:1521/ndssl.bioinformatics.vt.edu'
+		connStr = 'uname/pwd@url:port/sid'
 		db = cx_Oracle.connect(connStr)
 		cursor = db.cursor()
 
 		return cursor
 
 	elif db.lower() == 'wisdm':     #mysql
-		connStr = 'mysql://wisdmtest_user:test4ndssl@localhost/wisdmtest'
+		connStr = 'mysql://user:pwd@localhost/sid'
 		engine =  create_engine(connStr)
 		connection = engine.connect()
 
 		return connection
 
 	elif db.lower() == 'wisdmdev':	#oracle
-		connStr = 'oracle://wisdm_dev:change1t@noldor-db.vbi.vt.edu:1521/ndssl'
+		connStr = 'oracle://user:pwd@url:port/sid'
 		engine =  create_engine(connStr)
 		connection = engine.connect()
 
